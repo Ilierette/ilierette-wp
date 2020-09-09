3c607a0effe44ee42cc5a0bfc736691c416e636e
Ilierette <Ilierette@users.noreply.github.com>
2020-09-09 22:18:35 +0200

    simple wordpress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,16 @@ import { SimpleComponent } from 'components/component';
 import { useStores } from 'contexts';
 import { useObserver } from 'mobx-react';
 import * as React from 'react';
+const axios = require('axios');
 
 export const MainPage =() => {
   const { globalCtx } = useStores();
+
+  React.useEffect(()=>{
+    axios.get('http://a.ilierette.com/wp-json/wp/v2/posts').then((resp)=>{
+      console.log(resp.data)
+    })
+  })
 
   return useObserver(() => (
     <>
