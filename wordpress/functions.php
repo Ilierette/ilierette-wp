d3a8df21248e29e3d0c351b970af99b73a0a2c02
Karolina Pietraś <karolina.pietra@loyaltypoint.pl>
2020-09-23 09:30:16 +0200

    change

--- wordpress/functions.php
+++ wordpress/functions.php
@@ -2,15 +2,15 @@
     function register_my_menus() {
         register_nav_menus(
         array(
-            'header-menu' => __( 'Header Menu' ),
-            'extra-menu' => __( 'Extra Menu' )
+            'primary' => __( 'Primary' ),
+            'secondary' => __( 'Secondary' )
         )
         );
     }
     add_action( 'init', 'register_my_menus' );
     
     function custom_wp_menu() {
-        return wp_get_nav_menu_items('Header Menu');
+        return wp_get_nav_menu_items(5);
     }
     add_action( 'rest_api_init', function () {
         register_rest_route( 'wp/v2', 'menu', array(
