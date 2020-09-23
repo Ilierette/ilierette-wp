473dedec3eba8d1cbea84c8fb9052f02d4f36593
Ilierette <Ilierette@users.noreply.github.com>
2020-09-23 22:26:57 +0200

    yes

--- wordpress/index.php
+++ wordpress/index.php
@@ -3,6 +3,9 @@
 <head>
 <meta charset="<?php bloginfo('charset') ?>" />
 <?php wp_head() ?>
+<base href="/" />
+<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 </head>
 <body>
     <div id="root"></div>
