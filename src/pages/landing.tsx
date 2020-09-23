9f0bd058cfa7ec245e400b681a3d20c1d9c60ef4
Ilierette <Ilierette@users.noreply.github.com>
2020-09-23 23:12:13 +0200

    almooost

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -13,15 +13,22 @@ export const LandingPage = () => {
           <div className="description mb-4">Dancing nerd | Stage girl | Digital artist</div>
           <img src={require("../images/avatar.jpg")} className="card-img-inner" />
           <div className="socials">
-
-            <button className="btn btn-primary w-100"><i className="fab fa-twitch"></i> Watch stream </button>
+            <button className="btn btn-primary w-100 social-button">
+              <img src={require('../images/icons/twitch.svg')} className="btn-icon mr-2" />
+                Watch stream
+              </button>
           </div>
-
-
-          <Link to='/blog/'>Recent blog posts</Link>
+          <div className="socials">
+            <button className="btn btn-primary social-button"><img src={require('../images/icons/instagram.svg')} className="btn-icon" /></button>
+            <button className="btn btn-primary social-button"><img src={require('../images/icons/discord.svg')} className="btn-icon" /></button>
+            <button className="btn btn-primary social-button"><img src={require('../images/icons/youtube.svg')} className="btn-icon" /></button>
+            <button className="btn btn-primary social-button"><img src={require('../images/icons/instagram.svg')} className="btn-icon" /></button>
+            <button className="btn btn-primary social-button"><img src={require('../images/icons/artstation.svg')} className="btn-icon" /></button>
+            <Link to='/blog/' className="btn btn-primary social-button"><img src={require('../images/icons/wordpress.svg')} className="btn-icon" /></Link>
+          </div>
+          <div className="description mt-4">email: ilierette@gmail.com</div>
         </div>
       </div>
-
     </div>
 
   ))
