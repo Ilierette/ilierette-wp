642794005e688f5b458c76ebe4e459e9dd0a7e95
Karolina Pietraś <karolina.pietra@loyaltypoint.pl>
2020-09-24 11:38:26 +0200

    icons done

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -10,23 +10,29 @@ export const LandingPage = () => {
       <div className="card mx-auto">
         <div className="card-body">
           <h1>Ilierette</h1>
-          <div className="description mb-4">Dancing nerd | Stage girl | Digital artist</div>
+          <div className="description mb-4">Dancing nerd | Stage girl <br/> Digital artist</div>
           <img src={require("../images/avatar.jpg")} className="card-img-inner" />
           <div className="socials">
-            <button className="btn btn-primary w-100 social-button">
-              <img src={require('../images/icons/twitch.svg')} className="btn-icon mr-2" />
-                Watch stream
-              </button>
+            <a target="/blank" href="https://www.twitch.tv/ilieret" className="btn btn-primary social-button">
+              <div><img src={require('../images/icons/twitch.svg')} className="btn-icon mr-2" /></div>
+              <div>Watch stream</div>
+            </a>
+            <a target="/blank" href="https://www.youtube.com/channel/UCNkgXz2HcVk4KGMRnpD4rnQ/videos" className="btn btn-primary"><img src={require('../images/icons/youtube.svg')} className="btn-icon" /></a>
           </div>
           <div className="socials">
-            <button className="btn btn-primary"><img src={require('../images/icons/instagram.svg')} className="btn-icon" /></button>
-            <button className="btn btn-primary"><img src={require('../images/icons/discord.svg')} className="btn-icon" /></button>
-            <button className="btn btn-primary"><img src={require('../images/icons/youtube.svg')} className="btn-icon" /></button>
-            <button className="btn rainbow-button"><img src={require('../images/icons/instagram.svg')} className="btn-icon" /></button>
-            <button className="btn rainbow-button"><img src={require('../images/icons/artstation.svg')} className="btn-icon" /></button>
-            <Link to='/blog/' className="btn btn-primary"><img src={require('../images/icons/wordpress.svg')} className="btn-icon" /></Link>
+            <a target="/blank" href="https://www.instagram.com/ilierette/" className="btn btn-primary"><img src={require('../images/icons/instagram.svg')} className="btn-icon" /></a>
+            <a target="/blank" href="https://www.instagram.com/ilierette.art/" className="btn btn-primary social-button">
+              <div className="mr-2"><img src={require('../images/icons/instagram.svg')} className="btn-icon" /></div>
+              <div> ART </div>
+            </a>
+            <a target="/blank" href="https://www.artstation.com/ilierette" className="btn btn-primary"><img src={require('../images/icons/artstation.svg')} className="btn-icon" /></a>
+            <a target="/blank" href="https://discord.com/invite/RHfP7tj" className="btn btn-primary"><img src={require('../images/icons/discord.svg')} className="btn-icon" /></a>
           </div>
-          <div className="description mt-4">email: ilierette@gmail.com</div>
+          <div className="socials">
+            <a target="/blank" href="https://www.facebook.com/ilierette/" className="btn btn-primary"><img src={require('../images/icons/facebook.svg')} className="btn-icon" /></a>
+            <Link to='/blog/' className="btn btn-primary"><img src={require('../images/icons/wordpress.svg')} className="btn-icon" /> Visit blog </Link>
+            <a target="/blank" href="https://twitter.com/ilierette" className="btn btn-primary"><img src={require('../images/icons/twitter.svg')} className="btn-icon" /></a>
+          </div>
         </div>
       </div>
     </div>
