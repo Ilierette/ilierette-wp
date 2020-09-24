4eb996e67ea145b281ecd7c362e743175a5cf3b1
Karolina Pietraś <karolina.pietra@loyaltypoint.pl>
2020-09-24 13:54:02 +0200

    fully working

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -10,11 +10,11 @@ export const LandingPage = () => {
       <div className="card mx-auto">
         <div className="card-body">
           <h1>Ilierette</h1>
-          <div className="description mb-4">Dancing nerd | Stage girl <br/> Digital artist</div>
+          <div className="description mb-4">Dancing nerd | Stage girl <br/> Digital artist | React.js developer</div>
           <img src={require("../images/avatar.jpg")} className="card-img-inner" />
           <div className="socials">
             <a target="/blank" href="https://www.twitch.tv/ilieret" className="btn btn-primary social-button">
-              <div><img src={require('../images/icons/twitch.svg')} className="btn-icon mr-2" /></div>
+              <div><img src={require('../images/icons/twitch.svg')} className="btn-icon mr-3" /></div>
               <div>Watch stream</div>
             </a>
             <a target="/blank" href="https://www.youtube.com/channel/UCNkgXz2HcVk4KGMRnpD4rnQ/videos" className="btn btn-primary"><img src={require('../images/icons/youtube.svg')} className="btn-icon" /></a>
@@ -22,7 +22,7 @@ export const LandingPage = () => {
           <div className="socials">
             <a target="/blank" href="https://www.instagram.com/ilierette/" className="btn btn-primary"><img src={require('../images/icons/instagram.svg')} className="btn-icon" /></a>
             <a target="/blank" href="https://www.instagram.com/ilierette.art/" className="btn btn-primary social-button">
-              <div className="mr-2"><img src={require('../images/icons/instagram.svg')} className="btn-icon" /></div>
+              <div className="mr-3"><img src={require('../images/icons/instagram.svg')} className="btn-icon" /></div>
               <div> ART </div>
             </a>
             <a target="/blank" href="https://www.artstation.com/ilierette" className="btn btn-primary"><img src={require('../images/icons/artstation.svg')} className="btn-icon" /></a>
@@ -30,7 +30,9 @@ export const LandingPage = () => {
           </div>
           <div className="socials">
             <a target="/blank" href="https://www.facebook.com/ilierette/" className="btn btn-primary"><img src={require('../images/icons/facebook.svg')} className="btn-icon" /></a>
-            <Link to='/blog/' className="btn btn-primary"><img src={require('../images/icons/wordpress.svg')} className="btn-icon" /> Visit blog </Link>
+            <a target="/blank" href="https://www.linkedin.com/in/ilierette/" className="btn btn-primary"><img src={require('../images/icons/linkedin.svg')} className="btn-icon" /></a>
+            <Link to='/blog/' className="btn btn-primary"><img src={require('../images/icons/wordpress.svg')} className="btn-icon" /></Link>
+            <a target="/blank" href="https://github.com/Ilierette" className="btn btn-primary"><img src={require('../images/icons/github.svg')} className="btn-icon" /></a>
             <a target="/blank" href="https://twitter.com/ilierette" className="btn btn-primary"><img src={require('../images/icons/twitter.svg')} className="btn-icon" /></a>
           </div>
         </div>
--- wordpress/index.php
+++ wordpress/index.php
@@ -6,11 +6,11 @@
 <base href="/" />
 <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
 <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 </head>
 <body>
     <div id="root"></div>
 <?php wp_footer() ?>
-<?php echo getcwd(); ?>
   <script type="text/javascript" src="/react/index.js"></script>
 </body>
 </html>
